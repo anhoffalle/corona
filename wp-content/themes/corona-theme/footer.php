34521ae09e19da7c3b954345f2120497b976e961
anhoffalle <anhoffalle@gmail.com>
2026-02-19 21:50:57 +0200

    chore(i18n): wrap aria-label social links with esc_attr__

--- wp-content/themes/corona-theme/footer.php
+++ wp-content/themes/corona-theme/footer.php
@@ -57,21 +57,21 @@
 					?>
 					<div class="footer-social">
 						<?php if ( $social_links['twitter'] ) : ?>
-						<a href="<?php echo esc_url( $social_links['twitter'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="Twitter">
+						<a href="<?php echo esc_url( $social_links['twitter'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="<?php echo esc_attr__( 'Twitter', 'corona-theme' ); ?>">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="20" height="20">
 								<path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
 							</svg>
 						</a>
 						<?php endif; ?>
 						<?php if ( $social_links['facebook'] ) : ?>
-						<a href="<?php echo esc_url( $social_links['facebook'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="Facebook">
+						<a href="<?php echo esc_url( $social_links['facebook'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="<?php echo esc_attr__( 'Facebook', 'corona-theme' ); ?>">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="20" height="20">
 								<path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
 							</svg>
 						</a>
 						<?php endif; ?>
 						<?php if ( $social_links['instagram'] ) : ?>
-						<a href="<?php echo esc_url( $social_links['instagram'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="Instagram">
+						<a href="<?php echo esc_url( $social_links['instagram'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="<?php echo esc_attr__( 'Instagram', 'corona-theme' ); ?>">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="20" height="20">
 								<rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
 								<path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -80,7 +80,7 @@
 						</a>
 						<?php endif; ?>
 						<?php if ( $social_links['telegram'] ) : ?>
-						<a href="<?php echo esc_url( $social_links['telegram'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="Telegram">
+						<a href="<?php echo esc_url( $social_links['telegram'] ); ?>" class="social-link" target="_blank" rel="noopener" aria-label="<?php echo esc_attr__( 'Telegram', 'corona-theme' ); ?>">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="20" height="20">
 								<path d="M21.5 2.5l-19 9 6 2 2 6 4-4 5 4 2-17z"></path>
 							</svg>
